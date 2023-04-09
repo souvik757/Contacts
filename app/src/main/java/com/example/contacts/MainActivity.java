40531fc0a3b018046027a32b9db93c90035b8286
souvik757 <88132246+souvik757@users.noreply.github.com>
2023-04-09 14:14:02 +0530

    Initial commit

---
+++ app/src/main/java/com/example/contacts/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.contacts;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+
+// SH1 : 20:B8:5A:2D:A4:5C:A6:2C:67:DA:5A:E6:04:4E:DF:BC:0F:7B:90:55
